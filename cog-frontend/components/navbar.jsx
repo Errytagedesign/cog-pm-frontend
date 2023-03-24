21eb2aace70f811d073cbc315932c7458550d6b6
Ajibade Faruk <ajibadefarukyoungprof02@gmail.com>
2023-03-24 18:21:32 +0100

    Added The Navbar

---
+++ cog-frontend/components/navbar.jsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import logo1 from "../public/assets/icons/COG Logo.png";
+const Navbar = () => {
+  return (
+    <>
+      <nav className="flex justify-between px-24 items-center">
+        <div>
+          <Image src={logo1} alt="Cog Logo" />
+        </div>
+        <div className="w-3/6">
+          <ul className="flex justify-between ">
+            <li className="px-4">Home</li>
+            <li>For rentals</li>
+            <li>For property managers/owners</li>
+          </ul>
+        </div>
+        <div className="w-48">
+          <ul  className="flex justify-between items-center">
+            <li>About us</li>
+            <li className="bg-primary text-white px-5 py-2 rounded">Sign in</li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
