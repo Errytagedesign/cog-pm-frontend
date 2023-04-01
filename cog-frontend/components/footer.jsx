38c09a6a91e4c37153216be1777fab15ccfc46ad
Errytagedesign <errytage@gmail.com>
2023-04-01 11:22:53 +0100

    bug fixes

--- cog-frontend/components/footer.jsx
+++ cog-frontend/components/footer.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Image from "next/image";
-import Twitter from '../public/assets/icons/twitter.png'
-import instagram from '../public/assets/icons/instagram.png'
-import facebook from '../public/assets/icons/facebook.png'
-import email from '../public/assets/icons/email.png'
+import Twitter from "../public/assets/icons/twitter.png";
+import instagram from "../public/assets/icons/instagram.png";
+import facebook from "../public/assets/icons/facebook.png";
+import email from "../public/assets/icons/email.png";
 const Footer = () => {
   return (
-    <section className=" justify-between  m-auto bg-primary text-white">
+    <section className=" justify-between  m-auto w-full bg-primary text-white">
       <div className="w-3/4 flex justify-between m-auto py-8">
         <div className="">
           <div className="cursor-pointer">About Us</div>
@@ -30,10 +30,18 @@ const Footer = () => {
       </div>
       <div className="w-3/4 m-auto">
         <div className="flex mt-2">
-          <span className="bg-white p-2 m-2 rounded-full cursor-pointer"><Image src={Twitter} alt='Twitter Icon'/></span>
-          <span className="bg-white p-2 m-2 rounded-full cursor-pointer"><Image src={instagram} alt='Twitter Icon'/></span>
-          <span className="bg-white p-2 m-2 rounded-full cursor-pointer"><Image src={facebook} alt='facebook icon'/></span>
-          <span className="bg-white p-2 m-2 rounded-full cursor-pointer"><Image src={email} alt='email icon'/></span>
+          <span className="bg-white p-2 m-2 rounded-full cursor-pointer">
+            <Image src={Twitter} alt="Twitter Icon" />
+          </span>
+          <span className="bg-white p-2 m-2 rounded-full cursor-pointer">
+            <Image src={instagram} alt="Twitter Icon" />
+          </span>
+          <span className="bg-white p-2 m-2 rounded-full cursor-pointer">
+            <Image src={facebook} alt="facebook icon" />
+          </span>
+          <span className="bg-white p-2 m-2 rounded-full cursor-pointer">
+            <Image src={email} alt="email icon" />
+          </span>
         </div>
       </div>
     </section>
